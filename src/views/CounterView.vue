fb32758cc3b0133b15cfe48fde70856e26d610f3
Oscar <ocetars@gmail.com>
2025-02-01 19:05:52 +0800

    Add Counter route and navigation link

---
+++ src/views/CounterView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>计数器页面</h1>
+    <!-- 引入 CounterComponent -->
+    <CounterComponent />
+  </div>
+</template>
+
+<script setup>
+import CounterComponent from '@/components/CounterComponent.vue'
+</script>
+
+<style scoped>
+/* 可根据需要添加样式 */
+</style> 
